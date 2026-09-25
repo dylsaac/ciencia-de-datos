9a7d5d230864af03d4d6b522fe7cbbea3118e803
DYLAN ISAAC HEREDIA RODRIGUEZ <zS26004642@estudiantes.uv.mx>
2026-09-25 18:20:37 +0000

    hola plebe

---
+++ data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import streamlit as st
+
+names_link = ('https://raw.githubusercontent.com/dylsaac/Base-de-datos-Fortnite-Dylan/main/Fortnite_players_stats.csv')
+names_dataframe = pd.read_csv (names_dataframe)
+
+st ("Hola gente")
+st dataframe(names_data)
